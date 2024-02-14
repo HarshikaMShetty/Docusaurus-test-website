40f7c8b6a1de82baae2c7d3dd09284fb76b251ef
harshika.k-at-411977029542 <harshika.k@juegostudioz.com>
2024-02-14 15:08:04 +0530

    FEAT: token update

--- update_external_readme.js
+++ update_external_readme.js
@@ -77,7 +77,7 @@ function convertAndCopyFiles(sourceDir, destinationDir) {
 
 const repositoryUrls = [
   {
-    url: `https://HarshikaMShetty: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@github.com/HarshikaMShetty/Docusaurus-Test.git`,
+    url: `https://HarshikaMShetty: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@github.com/HarshikaMShetty/Docusaurus-Test.git`,
     branch: "test",
   },
 ];
